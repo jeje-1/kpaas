365e85c10cab5e19a5d7b8cc2ec970de5b068619
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-11-28 20:47:35 +0900

    git init

---
+++ src/test/java/mz/farmer/FarmerApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.farmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
